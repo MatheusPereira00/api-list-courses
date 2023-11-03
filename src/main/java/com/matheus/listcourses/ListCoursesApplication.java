0113a53c20763afab235f0263276fb4fb4c470c6
matheuspereira00 <matheusinhofcb12@gmail.com>
2023-11-03 09:54:36 -0300

    fix: refactored project

--- src/main/java/com/matheus/listcourses/ListCoursesApplication.java
+++ src/main/java/com/matheus/listcourses/ListCoursesApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ListCoursesApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ListCoursesApplication.class, args);
     }
-
 }
