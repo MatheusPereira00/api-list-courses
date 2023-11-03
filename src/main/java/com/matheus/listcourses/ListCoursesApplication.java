9e0f34c6fb3232376000ab6f4ab9d062e22fc8fb
matheuspereira00 <matheusinhofcb12@gmail.com>
2023-11-03 09:52:20 -0300

    first

---
+++ src/main/java/com/matheus/listcourses/ListCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.listcourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListCoursesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListCoursesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matheus/listcourses/ListCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.listcourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListCoursesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
